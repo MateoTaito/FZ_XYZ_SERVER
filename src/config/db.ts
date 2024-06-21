2d72de70624f9e101bb4f694c982e0a8e2fcab5d
MateoTaito <mtaito@alumnos.uai.cl>
2024-06-21 04:35:14 -0400

    DB FIX

--- src/config/db.ts
+++ src/config/db.ts
@@ -8,6 +8,12 @@ dotenv.config();
 const db = new Sequelize(process.env.DATABASE_URL!, {
 	models: [__dirname + "/../models/**.*.ts"],
 	logging: false,
+	dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // Usa true si tienes el certificado SSL correcto
+        }
+    }
 });
 
 export default db;
