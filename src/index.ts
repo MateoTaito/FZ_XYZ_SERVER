abfa76b6b0b00c31eb75fe8b327678e78cddd15f
MateoTaito <102261103+MateoTaito@users.noreply.github.com>
2024-06-25 00:19:10 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import server from "./server";
 import swaggerDocs from "./doc/swagger";
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 80;
 server.listen(port, () => {
 	console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\nApi Levantada");
 	swaggerDocs(server, port);
